a050c3d41d76c7c3bc2aafc7b4f7a0f89a2196e2
VodBox <dillon@vodbox.io>
2016-12-12 12:37:24 +1300

    Effectively Finalized

--- main.js
+++ main.js
@@ -1,22 +1,43 @@
 const {app, BrowserWindow} = require('electron');
 const path = require('path');
 const url = require('url');
+const fs = require('fs');
 
 let win
 
 function createWindow () {
-  win = new BrowserWindow({width: 1280, height: 720, frame: false});//, fullscreen: true});
+  fs.readFile('./display.txt', function(error, data) {
+    console.log(data);
+    options = {};
+    Object.assign(options, JSON.parse(data));
+    console.log(options);
+    if(options.width == undefined) {
+      options.width = 640;
+    }
+    if(options.height == undefined) {
+      options.height = 360;
+    }
+    if(options.fullscreen == undefined) {
+      options.fullscreen = false;
+    }
+    if(options.frame == undefined) {
+      options.frame = false;
+    }
+    console.log(options);
 
-  win.loadURL(url.format({
-    pathname: path.join(__dirname, 'index.html'),
-    protocol: 'file:',
-    slashes: true
-  }));
+    win = new BrowserWindow({width: options.width, height: options.height, frame: options.frame, fullscreen: options.fullscreen});
 
-  win.webContents.openDevTools({'mode': 'detach'});
+    win.loadURL(url.format({
+      pathname: path.join(__dirname, 'index.html'),
+      protocol: 'file:',
+      slashes: true
+    }));
 
-  win.on('closed', () => {
-    win = null
+    win.webContents.openDevTools({'mode': 'detach'});
+
+    win.on('closed', () => {
+      win = null
+    });
   });
 }
 
